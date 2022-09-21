657ddf1f590aed177e825830ce83fddb84548ba1
shorox2 <85540487+shorox2@users.noreply.github.com>
2022-09-21 18:10:04 +0300

    First commit

---
+++ webApp/scripts/main.js
@@ -0,0 +1,20 @@
+let switchMode = document.getElementById("switchMode");
+
+switchMode.onclick = function () {
+    let theme = document.getElementById("theme");
+    let button = document.getElementById("img-button")
+
+    if (theme.getAttribute("href") == "../assets/css/Light-mode.css") {
+        theme.href = "../assets/css/Dark-mode.css";
+        button.src = "../assets/img/sun-fill.svg"
+    } else {
+        theme.href = "../assets/css/Light-mode.css";
+        button.src = "../assets/img/moon-stars-fill.svg"
+    }
+}
+
+var myCarousel = document.querySelector('#carousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 2000,
+    wrap: false
+})
