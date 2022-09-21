39d4b4569c88bb5e9b0a8ba3a8572b370fecd980
shorox2 <85540487+shorox2@users.noreply.github.com>
2022-09-21 21:33:02 +0300

    Добавление остальных страниц html

--- webApp/scripts/main.js
+++ webApp/scripts/main.js
@@ -1,20 +1,20 @@
 let switchMode = document.getElementById("switchMode");
 
 switchMode.onclick = function () {
+    let nav = document.getElementsByClassName("navbar");
     let theme = document.getElementById("theme");
-    let button = document.getElementById("img-button")
+    let button = document.getElementById("img-button");
 
     if (theme.getAttribute("href") == "../assets/css/Light-mode.css") {
+        nav.classlist.toogle("dark-nav")
+        nav.classlist.toogle("bg-primary")
         theme.href = "../assets/css/Dark-mode.css";
         button.src = "../assets/img/sun-fill.svg"
     } else {
+        nav.classlist.toogle("bg-primary")
+        nav.classlist.toogle("dark-nav")
         theme.href = "../assets/css/Light-mode.css";
         button.src = "../assets/img/moon-stars-fill.svg"
     }
 }
 
-var myCarousel = document.querySelector('#carousel')
-var carousel = new bootstrap.Carousel(myCarousel, {
-    interval: 2000,
-    wrap: false
-})
