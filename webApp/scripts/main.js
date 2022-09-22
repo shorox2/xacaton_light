44955b7f6e9e3f814cd2ae1833875c9b9174572c
shorox2 <85540487+shorox2@users.noreply.github.com>
2022-09-22 04:31:31 +0300

    Add files via upload

--- webApp/scripts/main.js
+++ webApp/scripts/main.js
@@ -1,20 +1,28 @@
 let switchMode = document.getElementById("switchMode");
 
 switchMode.onclick = function () {
-    let nav = document.getElementsByClassName("navbar");
     let theme = document.getElementById("theme");
     let button = document.getElementById("img-button");
 
     if (theme.getAttribute("href") == "../assets/css/Light-mode.css") {
-        nav.classlist.toogle("dark-nav")
-        nav.classlist.toogle("bg-primary")
         theme.href = "../assets/css/Dark-mode.css";
         button.src = "../assets/img/sun-fill.svg"
     } else {
-        nav.classlist.toogle("bg-primary")
-        nav.classlist.toogle("dark-nav")
         theme.href = "../assets/css/Light-mode.css";
         button.src = "../assets/img/moon-stars-fill.svg"
+    }
+}
+
+
+let like = document.getElementById("flavour");
+
+like.onclick = function () {
+    let imgl = document.getElementById("her-l");
+
+    if (imgl.getAttribute("src") == "../assets/img/heart.svg") {
+        imgl.src = "../assets/img/heart-fill.svg"
+    } else {
+        imgl.src = "../assets/img/heart.svg"
     }
 }
 
